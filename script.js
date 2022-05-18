426128ebf7985c6b79511b8451f0fc6c9a531b93
Ryan Angela <ryan.c.angela@gmail.com>
2022-05-18 15:00:16 -0400

    start of project

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Retry Btn - refresh window
+ window.functionName= refreshPage;
+function refreshPage(){
+    window.location.reload();
+    refreshButton.addEventListener('click', refreshPage)
+} 
+
+
+const userGuess = function() {
+document.querySelector(`.guess`).addEventListener(`click`, checkNumber()
+  console.log(userGuess);
+}
